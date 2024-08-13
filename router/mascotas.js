1f958dfcf7d4fdbd89b26b35f802f1cc2269a36e
Damian Garcia <damiangt13@gmail.com>
2024-08-13 07:53:46 -0500

    Finalizacion CRUD desde la API

--- router/mascotas.js
+++ router/mascotas.js
@@ -2,6 +2,8 @@ const express = require('express');
 const Mascota = require('../models/mascota');
 const router = express.Router();
 
+/* insercion de datos desde la API */
+
 router.post("/mascotas", (req, res) => {
     //res.send("creacion de mascotas")
     const mascota = Mascota(req.body);
@@ -9,6 +11,46 @@ router.post("/mascotas", (req, res) => {
         .save()
         .then((data) => res.json(data))
         .catch((error) => res.json({message: error}));
+})
+
+/* consulta de datos desde la API */
+
+router.get("/mascotas", (req, res) => {
+    Mascota
+        .find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({message: error}))
+})
+
+/* consulta de un dato desde la API */
+
+router.get("/mascotas/:id", (req, res) => {
+    const {id} = req.params;
+    Mascota
+        .findById(id)
+        .then((data) => res.json(data))
+        .catch((error) => res.json({message: error}))
+})
+
+/* edicion de un dato desde la API */
+
+router.put("/mascotas/:id", (req, res) => {
+    const {id} = req.params;
+    const {nombre, descripcion} = req.body;
+    Mascota
+        .updateOne({_id: id}, {$set:{nombre, descripcion}})
+        .then((data) => res.json(data))
+        .catch((error) => res.json({message: error}))
+})
+
+/* eliminacion de un dato desde la API */
+
+router.delete("/mascotas/:id", (req, res) =>{
+    const{id} = req.params;
+    Mascota
+        .deleteOne({_id: id})
+        .then((data) => res.json(data))
+        .catch((error) => res.json({message: error}))
 })
 
 module.exports = router;
